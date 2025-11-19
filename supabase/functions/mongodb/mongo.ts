1be72a7198b7547e08f426d0499e4aa2d9276eeb
augargbladko <augargbladko@gmail.com>
2025-11-19 10:36:08 -0800

    Get first info from mongodb

---
+++ supabase/functions/mongodb/mongo.ts
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+
+// deno run --allow-all --env-file supabase/functions/mongodb/mongo.ts
+
+export async function MongoTest() {
+  console.log("DB URL:", Deno.env.get("DATABASE_URL"));
+  const client = new MongoClient(Deno.env.get("DATABASE_URL")!);
+  await client.connect();
+
+  const db = client.db("myFirstDatabase");
+  const tonPurchase = db.collection("TonPurchase");
+  const first = await tonPurchase.findOne({});
+  console.log("MongoDB Connection Test, first document:", first);
+  const result = await tonPurchase.aggregate([{
+      $group: {
+        _id: null,
+        total: {
+          $sum: "$tonPaid"
+        }
+      }
+  }]).toArray();
+  const tonSpent = (result[0]?.total || 0) / 1e9;
+  console.log("MongoDB Aggregation Result:", tonSpent);
+  Deno.exit();
+}
+
+MongoTest();
